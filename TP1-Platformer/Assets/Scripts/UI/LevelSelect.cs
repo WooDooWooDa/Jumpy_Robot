369643322e3f56212fd5a2f23d442bb1f987c37d
WooDooWooDa <jeremie-bouchard@hotmail.fr>
2021-09-09 20:32:47 -0400

    menus
    main menu
    level select menu

---
+++ TP1-Platformer/Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+    public Button[] levelButtons;
+
+    public void Awake()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+            {
+                levelButtons[i].interactable = false;
+            }
+        }
+    }
+    public void SelectLevel(string levelNumber)
+    {
+        SceneManager.LoadScene("Level" + levelNumber);
+    }
+}
