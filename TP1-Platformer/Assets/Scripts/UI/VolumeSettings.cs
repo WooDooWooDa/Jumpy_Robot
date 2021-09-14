3888c81b770ece78060eceb4582f2973129824f3
WooDooWooDa <jeremie-bouchard@hotmail.fr>
2021-09-13 22:53:19 -0400

    volume settings done

--- TP1-Platformer/Assets/Scripts/UI/VolumeSettings.cs
+++ TP1-Platformer/Assets/Scripts/UI/VolumeSettings.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
+    [SerializeField] private Slider volumeSlider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        volumeSlider.value = PlayerPrefs.GetFloat("masterVolume", 100f);
+        volumeSlider.onValueChanged.AddListener(ChangeVolume);
     }
 
-    // Update is called once per frame
-    void Update()
+    void ChangeVolume(float value)
     {
-        
-    }
+        PlayerPrefs.SetFloat("masterVolume", value);
+    } 
+
 }
